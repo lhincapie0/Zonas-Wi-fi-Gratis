b205fafe9ef1e869b2e2e1e0da57df9bc30c96f6
Laura Hincapie Calderon <lauhincapie97@gmail.com>
2019-02-01 16:48:03 -0500

    The Wi-fi zones data was saved in text files.

---
+++ Zonas Wifi/Zonas Wifi/ZonaW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zonas_Wifi
+{
+    class ZonaW
+    {
+        public String nombre;
+        public String municipio;
+        public String iD;
+        public String direccion;
+        public double longitud;
+        public double latitud;
+    }
+}
